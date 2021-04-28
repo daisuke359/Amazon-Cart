1a2ac12f74f5e75dc530c762421e2c74faa82a96
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-04-27 19:56:43 -0700

    Change format of total price

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../components/CartTotal.css';
+import NumberFormat from 'react-number-format';
 
 function CartTotal({items}) {
 
@@ -9,13 +10,24 @@ function CartTotal({items}) {
             total += (item.price * item.quantity);
         });
 
-        return total.toFixed(2);
+        return total;
+    }
+
+    const getTotalItems = () => {
+        let total =0;
+        items.forEach(item => {
+            total += item.quantity;
+        });
+
+        return total;
     }
 
     return (
         <div className="CartTotal">
-            <h3>Subtotal({items.length} items): 
-                <span className="CartTotal-price">{getTotalPrice()}</span>
+            <h3>Subtotal({getTotalItems()} items): 
+                <span className="CartTotal-price">
+                    <NumberFormat value={getTotalPrice()} displayType={"text"} thousandSeparator={true} prefix={"$"} decimalScale={2} />
+                </span>
             </h3>
             <button>Proceed to checkout</button>
         </div>
